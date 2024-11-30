cde132aeeb548123fcbbc639cdaeb2380e54943c
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 17:12:11 +0000

    Create placeholder for segment randomisation

--- scripts/segment/segment.gd
+++ scripts/segment/segment.gd
@@ -1,12 +1,25 @@
 class_name Segment
 extends Node2D
 
+const GRID_WIDTH: int = 6
+const GRID_HEIGHT: int = 5
+
 @onready
-var _interaction_grid: TileMapLayer = $Interactions
+var _building_layout: TileMapLayer = $Building
 
 func _ready() -> void:
 	randomise_layout()
 
 func randomise_layout() -> void:
-	# TODO: Randomise the layouts and validate that you can complete them
-	pass
+	for x in GRID_WIDTH:
+		for y in GRID_HEIGHT:
+			var window: int = randi_range(0, 1)
+			_building_layout.set_cell(
+				Vector2i(x, y), 
+				1, 
+				Vector2i(window, 0)
+			)
+			
+			if window == 0:
+				# Window was spawned, can create interactable
+				pass
