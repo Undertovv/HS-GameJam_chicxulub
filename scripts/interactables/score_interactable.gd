49652ad5839557b312a5a8b1249e8994e12e5cba
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 22:05:41 +0000

    Add exploding cat

--- scripts/interactables/score_interactable.gd
+++ scripts/interactables/score_interactable.gd
@@ -4,6 +4,14 @@ extends Interactable
 @export
 var score: int
 
+@export
+var _animation_player: AnimationPlayer
+
 func run_interaction() -> void:
+	monitorable = false
 	GameState.get_instance().add_score(score)
+	
+	_animation_player.play("explode")
+	await _animation_player.animation_finished
+	
 	queue_free()
