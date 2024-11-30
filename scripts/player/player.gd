bbdd67aa4e809a25d66cd8235216e963a1d735fe
Megan "Evil" Welter <em.welter@protonmail.com>
2024-11-30 15:19:11 +0000

    we redoin movement :3

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,26 +1,19 @@
-extends CharacterBody2D
+extends Node2D
 
-
-const SPEED = 20.0
+@export
+var SPEED = 20.0
 
 func _physics_process(delta: float) -> void:
-	
-	# Gravity is set to 0 because it's space-themed :3
-	# (don't) add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
 # Get his ass movin AND schmoovin.
 	var yDirection := Input.get_axis("pivotUp", "pivotDown")
 	if yDirection:
-		velocity.y = yDirection * SPEED
+		translate(Vector2(0, 0) * SPEED) # Go North
 	else:
-		velocity.y = yDirection * SPEED
+		translate(Vector2(0, 1) * SPEED) # Go South
 
 	var xDirection := Input.get_axis("pivotLeft", "pivotRight")
 	if xDirection:
-		velocity.x = xDirection * SPEED
+		translate(Vector2(1, 0) * SPEED) # Go East
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		translate(Vector2(1, 1) * SPEED) # Go West
