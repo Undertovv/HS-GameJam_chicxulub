23c35ca373989e2b7867d375882c91aeb0493039
Megan "Evil" Welter <em.welter@protonmail.com>
2024-11-30 16:13:56 +0000

    Improved movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-@export
-var speed = 2.0
+@export var speed = 2.0
 var direction = Vector2(0, 0)
 
 	#Get his ass movin AND schmoovin.
 func _process(delta: float) -> void:
-	direction = Input.get_vector("pivotLeft","pivotRight","pivotUp","pivotDown")
+	var input: Vector2 = Input.get_vector("pivotLeft","pivotRight","pivotUp","pivotDown").normalized()
+	if direction != input and input != Vector2(0,0):
+		direction = input
+		
+	#For testing, remove later
 	schmoove(direction)
 
 func schmoove(direction):
-	#translate movement collected from proc to translate functions
-	#translate functions will be called when ready
+	#main movement function, translates movement collected from proc
 	translate(direction * speed);
