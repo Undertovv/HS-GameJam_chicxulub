dd13a7d1802f8e1d1d1dd8abdb7e6f70966f22fc
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 13:56:21 +0000

    Implement player death system

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -22,3 +22,12 @@ func add_score(score: int) -> void:
 	_current_score += score
 	score_changed.emit(_current_score)
 #endregion
+
+#region Death
+## Emmitted when the player has collided with a wall or itself
+signal player_died
+
+## Kills the player
+func johnathon() -> void:
+	player_died.emit()
+#endregion
