e6924cc7391e67818fd5e482a8fd1fdfd911f601
BillyPutDev <billyputland@hotmail.com>
2024-11-30 18:12:25 +0000

    Waypoints

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 @export
-var speed = 100
-var direction = Vector2(0, -1)
+var speed = 25
+var direction = Vector2(0, 1)
+var secureDirection = Vector2(0, 1)
+var wayPoint = Vector2(0, 0)
+var distance : float
+
+func _ready() -> void:
+	wayPoint = Vector2(self.position.x, self.position.y + 240)
 
 # Get his ass movin AND schmoovin.
 func _process(delta: float) -> void:
@@ -14,9 +20,24 @@ func _process(delta: float) -> void:
 		direction = Vector2(0, 1)
 	if Input.is_key_pressed(KEY_D):
 		direction = Vector2(1, 0)
-		
-	shmoeve(direction, delta)
 	
+	
+	if self.position.distance_to(wayPoint) < 5:
+		
+		self.position = wayPoint
+		
+		if direction == Vector2(0, -1):
+			wayPoint = self.position + Vector2(0, -240)
+		if direction == Vector2(-1, 0):
+			wayPoint = self.position + Vector2(-240, 0)
+		if direction == Vector2(0, 1):
+			wayPoint = self.position + Vector2(0, 240)
+		if direction == Vector2(1, 0):
+			wayPoint = self.position + Vector2(240, 0)
+			
+		secureDirection = direction
+		
+	shmoeve(secureDirection, delta)
 
 func shmoeve(dir: Vector2, delta: float):
 	translate(dir * speed * delta)
