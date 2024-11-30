d321cee34f6b8e5d9fd96a84a76894028a3095df
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 15:02:05 +0000

    Add person_normal to interactions tilleset
    Also fix issues

--- scripts/interactables/interactable.gd
+++ scripts/interactables/interactable.gd
@@ -1,9 +1,6 @@
 class_name Interactable
 extends Area2D
 
-func _ready() -> void:
-	monitoring = false
-
 ## Called by the player object when an interaction needs to be run
 func run_interaction() -> void:
 	pass
