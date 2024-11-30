b1db9671c8d1c4c36487fab91e95d251550ab023
Megan "Evil" Welter <em.welter@protonmail.com>
2024-11-30 19:09:17 +0000

    wii party themed sexting

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,10 +4,10 @@ extends Node2D
 var direction = Vector2(0, 1)
 var secureDirection = Vector2(0, 1)
 var wayPoint = Vector2(0, 0)
-@export var fucking = 16
-var distanceTraveled = 0
-const shaft = preload("res://sprites/blehCat.png")
-
+@export var fucking = 16 #Distance between waypoints
+var distanceTraveled = 0 ##Used for tracking trail sprite laying
+const vertShaft = preload("res://sprites/Dino/VerticalMovementNeck.png")
+const horiShaft = preload("res://sprites/Dino/DinoNeckHorizontal.png")
 func _ready() -> void:
 	wayPoint = Vector2(self.position.x, self.position.y + fucking)
 
@@ -27,13 +27,7 @@ func _process(delta: float) -> void:
 		
 		self.position = wayPoint
 		distanceTraveled+=1
-		
-		if distanceTraveled % 3 == 1:
-			var sprite = Sprite2D.new()
-			sprite.texture = shaft
-			sprite.scale = Vector2(0.2, 0.2)
-			sprite.position = self.position
-			self.get_parent().add_child(sprite)
+
 		
 		if direction == Vector2(0, -1):
 			wayPoint = self.position + Vector2(0, -fucking)
@@ -46,12 +40,19 @@ func _process(delta: float) -> void:
 	
 		secureDirection = direction
 	
-	shmoeve(secureDirection, delta)
+	shmoeve(secureDirection, delta) #move and/or shmove
 
 	
 
 func shmoeve(dir: Vector2, delta: float):
 	translate(dir * speed * delta)
+	
+func trail(direction): #change trail sprite for direction
+	var sprite = Sprite2D.new()
+	sprite.texture = direction
+	sprite.scale = Vector2(0.2, 0.2)
+	sprite.position = self.position
+	self.get_parent().add_child(sprite)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
