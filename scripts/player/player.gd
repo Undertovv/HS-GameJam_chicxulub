4fa42107e8d038ab015d05aa31de7151e9f2926e
BillyPutDev <billyputland@hotmail.com>
2024-11-30 16:26:29 +0000

    Shomoeving with keys

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,19 +1,27 @@
 extends Node2D
 
 @export
-var SPEED = 20.0
-
-func _physics_process(delta: float) -> void:
+var speed = 100
+var direction = Vector2(0, -1)
 
 # Get his ass movin AND schmoovin.
-	var yDirection := Input.get_axis("pivotUp", "pivotDown")
-	if yDirection:
-		translate(Vector2(0, 0) * SPEED) # Go North
-	else:
-		translate(Vector2(0, 1) * SPEED) # Go South
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_W):
+		direction = Vector2(0, -1)
+	if Input.is_key_pressed(KEY_A):
+		direction = Vector2(-1, 0)
+	if Input.is_key_pressed(KEY_S):
+		direction = Vector2(0, 1)
+	if Input.is_key_pressed(KEY_D):
+		direction = Vector2(1, 0)
+		
+	shmoeve(direction, delta)
+	
 
-	var xDirection := Input.get_axis("pivotLeft", "pivotRight")
-	if xDirection:
-		translate(Vector2(1, 0) * SPEED) # Go East
-	else:
-		translate(Vector2(1, 1) * SPEED) # Go West
+func shmoeve(dir: Vector2, delta: float):
+	translate(dir * speed * delta)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area is Interactable:
+		area.run_interaction()
