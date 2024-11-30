db0dc4d6d537792b5904936d9ae9aa2caa6acdda
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 18:28:08 +0000

    Implement the fire
    I want to burn this place to the ground /hj

--- scripts/interactables/death_interactable.gd
+++ scripts/interactables/death_interactable.gd
@@ -1,5 +1,11 @@
 class_name DeathInteractable
 extends Interactable
 
+@export
+var _fire: AnimatedSprite2D
+
+func _ready() -> void:
+	_fire.play("default")
+
 func run_interaction() -> void:
 	GameState.get_instance().johnathon()
