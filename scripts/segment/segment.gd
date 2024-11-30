12419a88c6491949fcd5d3fd681694e72a1003dc
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 22:29:50 +0000

    Comment segment.gd

--- scripts/segment/segment.gd
+++ scripts/segment/segment.gd
@@ -18,7 +18,10 @@ func _ready() -> void:
 func randomise_layout() -> void:
 	for x in GRID_WIDTH:
 		for y in GRID_HEIGHT:
+			# Cells with a window have a value of 0
 			var window: int = randi_range(0, 1)
+			
+			# Spawn the correct wall type at (x, y). Wall type is sources from _building_layout.tile_set 
 			_building_layout.set_cell(
 				Vector2i(x, y), 
 				1, 
@@ -26,9 +29,11 @@ func randomise_layout() -> void:
 			)
 			
 			if window == 0:
+				# Generate a random ID for the interaction. 0 is none, the rest represent a tile ID in _interactions.tile_map 
 				var interaction_id: int = randi_range(0, LAST_INTERACTABLE_ID)
 				
 				if interaction_id != 0:
+					# Spawm the correct interaction scene above the window
 					_interactions.set_cell(
 							Vector2i(x, y), 
 							0, 
@@ -38,6 +43,7 @@ func randomise_layout() -> void:
 						
 	if not _validate_layout():
 		randomise_layout()
-						
+
+## Validates the layout of the segment to ensure it can be beaten
 func _validate_layout() -> bool:
 	return true
