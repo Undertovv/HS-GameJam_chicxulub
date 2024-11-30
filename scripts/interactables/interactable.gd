766fbd8bd4862ad60f9bf588ebfe243fd9c7efc5
Neo Salmon <52429194+jds691@users.noreply.github.com>
2024-11-30 14:35:33 +0000

    Disable interactable.gd Area2D monitoring

--- scripts/interactables/interactable.gd
+++ scripts/interactables/interactable.gd
@@ -1,6 +1,9 @@
 class_name Interactable
 extends Area2D
 
+func _ready() -> void:
+	monitoring = false
+
 ## Called by the player object when an interaction needs to be run
 func run_interaction() -> void:
 	pass
